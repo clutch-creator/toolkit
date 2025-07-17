dbc78755a01fa56aa4af528b6e3ada62a5a98f18
Bruno Mota <bruno12mota@gmail.com>
2025-07-17 11:38:30 +0100

    feat: added cloneChildren util

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,5 @@
+import React, { useCallback } from 'react';
+
 export const getEntryInstanceId = (properties, entryInstances) => {
   const entry =
     entryInstances.find(entry => {
@@ -10,3 +12,66 @@ export const getEntryInstanceId = (properties, entryInstances) => {
 
   return entry?.id;
 };
+
+function mergeProps(slotProps, childProps) {
+  const overrideProps = { ...childProps };
+
+  for (const propName in childProps) {
+    const slotPropValue = slotProps[propName];
+    const childPropValue = childProps[propName];
+    const isHandler = /^on[A-Z]/.test(propName);
+
+    if (isHandler) {
+      if (slotPropValue && childPropValue) {
+        overrideProps[propName] = (...args) => {
+          childPropValue(...args);
+          slotPropValue(...args);
+        };
+      } else if (slotPropValue) {
+        overrideProps[propName] = slotPropValue;
+      }
+    } else if (propName === 'style') {
+      overrideProps[propName] = { ...slotPropValue, ...childPropValue };
+    } else if (propName === 'className') {
+      overrideProps[propName] = [slotPropValue, childPropValue]
+        .filter(Boolean)
+        .join(' ');
+    }
+  }
+
+  return { ...slotProps, ...overrideProps };
+}
+
+export function cloneChildren(children, { ...props }) {
+  if (Object.keys(props).length === 0) {
+    return children;
+  }
+
+  delete props['debug-id'];
+  delete props['debug-reports'];
+  delete props['debug-parent'];
+  delete props['debug-loop'];
+  delete props['debug-stop'];
+  delete props['debug-is-section'];
+  delete props['debug-name'];
+  delete props['data-d'];
+
+  const cloneChild = useCallback((child, index = undefined) => {
+    if (React.isValidElement(child)) {
+      const clonedElement = React.cloneElement(child, {
+        key: index,
+        ...mergeProps(child.props, props),
+      });
+
+      delete props.ref;
+
+      return clonedElement;
+    } else {
+      return child;
+    }
+  }, []);
+
+  return Array.isArray(children)
+    ? children.map(cloneChild)
+    : cloneChild(children);
+}
